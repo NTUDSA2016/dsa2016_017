1c3bc1e7b6900991d746273778d6e1409e0d8045
linnil1 <linnil1.886@gmail.com>
2016-03-21 00:05:05 +0800

    do much modify for quicker

--- HW2/q2.cpp
+++ HW2/q2.cpp
@@ -1,4 +1,14 @@
 #include<bits/stdc++.h>
+//46sec for linear
+//time is increasing
+//94 for one sort
+//218 for four sort
+//145 for two sort
+// 560 for uit sort
+// 160 for stable rowdata 1 sort
+// max 120 avg 29 per time
+// max 14456 avg 52 per user
+// max 8375857 avg 15543.370913 per item
 using std::string;
 using std::cout;using std::cin;
 
@@ -8,24 +18,22 @@ struct rowdata
 };
 
 #define VI std::vector<int> 
+//#define M 73209277
+#define M 20000
 class mydata
 {
 private:
-	std::vector<rowdata> vrowdata;
-	int n;//size
-	VI uit;// user item time 
-	VI itu;
-	VI iut;
+	rowdata* vrowdata;
+	std::vector<rowdata*> sortu ,sorti;// user item time 
+	std::set<int> issortu;
 	
 	void filein(string &s);
-	bool uitcomp(const int &,const int &)const;
-	bool itucomp(const int &,const int &)const;
-	bool iutcomp(const int &,const int &)const;
 
 public:
 	
 	//init
 	mydata(string);
+	~mydata(){delete[] vrowdata;};
 
 	int accept(int&,int&,int&);//0 not found 
 	VI items(int&,int&);
@@ -35,6 +43,28 @@ public:
 
 };
 
+inline bool compu( rowdata *a, rowdata *b)
+{
+	return a->user < b->user;
+}
+inline bool compi( rowdata *a, rowdata *b)
+{
+	return a->item < b->item;
+}
+
+inline bool comput( rowdata *a, rowdata *b)
+{
+	if( a->user != b->user)
+		return a->user < b->user;
+	return a->time < b->time;
+}
+inline bool compit( rowdata *a, rowdata *b)
+{
+	if( a->item !=  b->item)
+		return a->item < b->item;
+	return a->time < b->time;
+}
+
 void mydata::filein(string &s)
 {
 	FILE *f=fopen(s.c_str(),"r");
@@ -43,129 +73,94 @@ void mydata::filein(string &s)
 		puts("throw file error");
 		return ;
 	}
-	vrowdata = std::vector<rowdata>() ;
-	int u,i,r,t;
+	int u,i,r,t,j=0;
+	vrowdata = new rowdata [M];
 	while( ~fscanf(f,"%d%d%d%d",&u,&i,&r,&t) )
-		vrowdata.push_back( rowdata{u,i,t,r} );
+		vrowdata[j++] = ( rowdata{u,i,t,r} );
 	fclose(f);
 };
 
 #define mybind(a) std::bind(&a,this,std::placeholders::_1,std::placeholders::_2)
 mydata::mydata(string s="data.txt") 
 {
+	issortu= std::set<int>();
 	filein(s);
-	n = vrowdata.size();
-	vrowdata.resize(n+1);// a tmp on the back
+	printf("%f file ok\n",((float)clock()/CLOCKS_PER_SEC));
 
-	uit.resize(n);
-	itu.resize(n);
-	iut.resize(n);
-	for(int i=0;i<n;++i)
-		uit[i]=itu[i]=iut[i]=i;
+	sortu.resize(M);
+	sorti.resize(M);
+	for(int i=0;i<M;++i)
+		sortu[i] = sorti[i] = vrowdata+i;
 
 	// to my data
-	std::sort(uit.begin(),uit.end(),mybind(mydata::uitcomp));
-	std::sort(itu.begin(),itu.end(),mybind(mydata::itucomp));
-//	for(int i=0;i<itu.size();++i)
-//		printf("%d %d %d\n",vrowdata[itu[i]].user,vrowdata[itu[i]].item,vrowdata[itu[i]].time);
-	std::sort(iut.begin(),iut.end(),mybind(mydata::iutcomp));
+	std::stable_sort(sortu.begin(),sortu.end(),compu);
+	printf("%f u ok\n",((float)clock()/CLOCKS_PER_SEC));
+	std::stable_sort(sorti.begin(),sorti.end(),compi);
+	printf("%f i ok\n",((float)clock()/CLOCKS_PER_SEC));
 }
 
-bool mydata::uitcomp(const int &a,const int &b)const
-{
-	const rowdata &ra = vrowdata[a],
-				  &rb = vrowdata[b];
-
-	if(ra.user != rb.user)
-		return ra.user < rb.user;
-	if(ra.item != rb.item )
-		return ra.item < rb.item;
-	if(ra.time != rb.time)
-		return ra.time < rb.time;
-	return 0;
-}
-bool mydata::itucomp(const int &a,const int &b)const
-{
-	const rowdata &ra = vrowdata[a],
-				  &rb = vrowdata[b];
-
-	if(ra.item != rb.item )
-		return ra.item < rb.item;
-	if(ra.time != rb.time)
-		return ra.time < rb.time;
-	if(ra.user != rb.user)
-		return ra.user < rb.user;
-	return 0;
-}
-bool mydata::iutcomp(const int &a,const int &b)const
-{
-	const rowdata &ra = vrowdata[a],
-				  &rb = vrowdata[b];
-
-	if(ra.item != rb.item )
-		return ra.item < rb.item;
-	if(ra.user != rb.user)
-		return ra.user < rb.user;
-	if(ra.time != rb.time)
-		return ra.time < rb.time;
-	return 0;
-}
 
 int mydata::accept(int&u, int&i, int &t)
 {
-	vrowdata.back()=  rowdata{u,i,t,0} ;
-	auto now = std::lower_bound(uit.begin(),uit.end(),n,mybind(mydata::uitcomp));
+	rowdata here{u,i,t,0} ;
+	auto now = std::lower_bound(sortu.begin(),sortu.end(),&here,comput);
 
-	if( now == uit.end() || uitcomp(n,*now) || uitcomp(*now,n)  )
-		return 0;
-	else
-		return vrowdata[ *now ].result;
+	while( (*now)->user ==u && (*now)->time == t )
+	{
+		if( (*now)->item ==i)
+			return (*now)->result;
+		++now;
+	}
+	return 0;//not find
+
 }
 
 VI mydata::items(int &u1,int& u2)
 {
+	rowdata here{u1,0,0,0} ;
+	auto s1 = std::lower_bound(sortu.begin(),sortu.end(),&here,compu);
+	here.user=u2;
+	auto s2 = std::lower_bound(sortu.begin(),sortu.end(),&here,compu);
+	
 	VI ans;
-	vrowdata.back() =  rowdata{u1,0,0,0} ;
-	auto s1= std::lower_bound(uit.begin(),uit.end(),n,mybind(mydata::uitcomp));
-	vrowdata.back().user=u2;
-	auto s2= std::lower_bound(uit.begin(),uit.end(),n,mybind(mydata::uitcomp));
 
-	while( s1<uit.end() && s2<uit.end() &&vrowdata[*s1].user ==u1 && vrowdata[*s2].user==u2)
-		if(vrowdata[*s1].item < vrowdata[*s2].item)
+	while( s1<sortu.end() && s2<sortu.end() &&(*s1)->user ==u1 && (*s2)->user == u2)
+		if((*s1)->item < (*s2)->item)
 			++s1;
-		else if(vrowdata[*s1].item > vrowdata[*s2].item)
+		else if((*s1)->item > (*s2)->item)
 			++s2;
 		else
 		{
-			ans.push_back(vrowdata[*s1].item);
+			ans.push_back((*s1)->item);
 			++s1;++s2;
 		}
 
 	return ans;
 }
 
+
 VI mydata::users(int &i1,int &i2,int &t1,int &t2)
 {
 	VI ans;
 	std::set<int> set;
 	
-	vrowdata.back() = rowdata{0,i1,t1,0};
-	auto s1= std::lower_bound(itu.begin(),itu.end(),n,mybind(mydata::itucomp));
-	vrowdata.back() = rowdata{0,i1,t2+1,0};
-	while(s1<itu.end() && itucomp(*s1,n))
-		set.insert(vrowdata[*s1++].user);
+	rowdata here{0,i1,t1,0};
+	auto s = std::lower_bound(sorti.begin(),sorti.end(),&here,compit);
+	here.time = t2+1;
+	while(s<sorti.end() && compu(*s,&here))
+		set.insert((*s++)->user);
 	
-	vrowdata.back() = rowdata{0,i2,t1,0};
-	auto s2= std::lower_bound(itu.begin(),itu.end(),n,mybind(mydata::itucomp));
-	vrowdata.back() = rowdata{0,i2,t2+1,0};
-	while(s2<itu.end() && itucomp(*s2,n))
+	here={0,i2,t1,0};
+	s = std::lower_bound(sorti.begin(),sorti.end(),&here,compit);
+	here.time = t2+1;
+	while(s<sorti.end() && compu(*s,&here))
 	{
-		if(set.count(vrowdata[*s2].user))
+		if(set.count((*s)->user))
 		{
-			ans.push_back(vrowdata[*s2].user);
-			set.erase(vrowdata[*s2].user);// repeat user
+			ans.push_back((*s)->user);
+			set.erase((*s)->user);// repeat user
 		}
-		++s2;
+		++s;
 	}
 
 	std::sort(ans.begin(),ans.end());
@@ -176,13 +171,13 @@ VI mydata::ratio(int &it,int &hold)
 {
 	std::map<int,int> map;
 	std::set<int> set;
-	vrowdata.back() = rowdata{0,it,0,0};
-	auto s= std::lower_bound(iut.begin(),iut.end(),n,mybind(mydata::iutcomp));
-	while(s<iut.end() && vrowdata[*s].item == it)
+	rowdata here{0,it,0,0};
+	auto s = std::lower_bound(sorti.begin(),sorti.end(),&here,compi);
+	while(s<sorti.end() && (*s)->item == it)
 	{
-		++map[ vrowdata[*s].user ]  ;
-		if( vrowdata[*s].result == 1)
-			set.insert(vrowdata[*s].user);
+		++map[ (*s)->user ]  ;
+		if( (*s)->result == 1)//accept
+			set.insert((*s)->user);
 		++s;
 	}
 
@@ -197,27 +192,36 @@ VI mydata::ratio(int &it,int &hold)
 		}
 	return ans;
 }
-	
+
 
 VI mydata::findtime_item(int& it,VI& us)
 {
 	std::set<int> set;
-	vrowdata.back() = rowdata{0,it,0,0};
+	rowdata here{0,it,0,0};
+	auto is = std::lower_bound(sorti.begin(),sorti.end(),&here,compi),
+		 ie = std::upper_bound(sorti.begin(),sorti.end(),&here,compi);
+	
+	if(!issortu.count(it)){ // is sorted ?
+		std::stable_sort(is,ie,compu);
+		issortu.insert(it);
+	}
+
 	for(int &i:us)
 	{
-		vrowdata.back().user = i ;
-		auto s= std::lower_bound(iut.begin(),iut.end(),n,mybind(mydata::iutcomp));
-		while(s<iut.end() && vrowdata[*s].user==i && vrowdata[*s].item == it)
-			set.insert( vrowdata[*s++].time );
+		here.user = i ;
+		auto s = std::lower_bound(is,ie,&here,compu);
+		while(s<ie && (*s)->user==i )
+			set.insert( (*s++)->time );
 	}
 
 	return VI(set.begin(),set.end());
 }
 
+
 int main()
 {
 	mydata my;
-//	mydata my("/tmp2/KDDCUP2012/track1/rec log train.txt");
+//	mydata my("/tmp2/KDDCUP2012/track1/rec_log_train.txt");
 	int mode;
 	while( ~scanf("%d",&mode) )
 	{
