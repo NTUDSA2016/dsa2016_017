800fc037e42d1d6d72970e7a5db636c9af688fe4
linnil1 <linnil1.886@gmail.com>
2016-06-11 07:04:28 +0800

    spec changed

--- project/project.cpp
+++ project/project.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 
 #define DataMax 50000000
+//#define DataMax 500
 #define HashMult 2350001
 #define HashMod 99999989
 #define WordMult 11261
@@ -9,46 +10,175 @@
 #define ll long long 
 using std::string;
 
+struct Trie
+{
+	bool exist;
+	Trie* ch[26];
+	Trie():exist(0){
+		memset(ch,0,sizeof(ch));
+	}
+
+
+}*root;
+
+void preposInsert(char *str)
+{
+	Trie *now = root;
+	for(int i=0;str[i];++i)
+	{
+		char c = str[i]-'a';
+		if( now->ch[c] )
+			now = now->ch[c];
+		else
+			now = now->ch[c] = new Trie;
+	}
+	now->exist=1;
+}
+
+bool preposCheck(char *str,int& len)
+{
+	Trie *now = root;
+	for(int i=0;i<len;++i)
+	{
+		now = now->ch[str[i]-'a'];
+		if(!now) return 0;
+	}
+	return now->exist;
+}
+
+void preposInit()
+{
+	const int prepos_n = 20;
+	char prepos_c[prepos_n][10]= {
+		"of", "to", "in", "for", "with", 
+		"on", "at","by", "from", "up", 
+		"about", "than", "after", "before", "down", 
+		"between", "under", "since", "without", "near"};
+
+	root = new Trie;
+	for(int i=0;i<prepos_n;++i)
+		preposInsert(prepos_c[i]);
+}
+
 struct Sentence
 {
 	char str[220];
 	int feq;
-}*ori_data = new Sentence [DataMax];
-int ori_n=0;
 
-bool sentComp(Sentence *a,Sentence *b)
+	int noprep_n,
+		noprep_num[6];
+	
+	int words_n,
+		words_pos[10],
+		words_len[10];
+
+	void sentPrint()
+	{
+		puts("----------------------");
+		printf("n%d\n",noprep_n);
+		for(int i=0;i<words_n;++i)
+			printf("%d %d\n",words_pos[i],words_len[i]);
+		for(int i=0;i<noprep_n;++i)
+			printf("%d\n",noprep_num[i]);
+		printf("%s\t%d\n",str,feq);
+	}
+
+
+}*ori_data = new Sentence [DataMax];
+int ori_n;
+
+bool sentEq(const Sentence *a,const Sentence *b)
 {
-	if( a->feq != b->feq)
-		return a->feq > b->feq;
-	else
-		return a < b;
+	if(a->noprep_n != b->noprep_n)
+		return 0;
+	for(int i=0;i<a->noprep_n;++i)
+	{
+		if(a->words_len[a->noprep_num[i]]!=b->words_len[b->noprep_num[i]])
+			return 0;
+		int len = a->words_len[a->noprep_num[i]];
+		int pos = a->words_pos[a->noprep_num[i]],
+			posb= b->words_pos[b->noprep_num[i]];
+		for(int j=0;j<len;++j)
+			if(a->str[pos+j] != b->str[posb+j])
+				return 0;
+	}
+	return 1;
+}
+
+bool sentComp(const Sentence *a,const Sentence *b)
+{
+	return a->feq > b->feq;
+}
+
+char stop='\t';
+int sentDeal(Sentence *s)// modify origin string
+{
+	char *c = s->str;
+	
+	// get the size of each word
+	s->words_n = 0;
+	int start=-1,i;
+	for(i=0;c[i]!=stop;++i)
+		if( c[i] != ' ')
+		{
+			if(start==-1)
+				start=i;
+		}
+		else 
+		{
+			s->words_pos[s->words_n  ]=  start;
+			s->words_len[s->words_n++]=i-start;
+			start=-1;
+		}
+	s->words_pos[s->words_n  ]=  start;
+	s->words_len[s->words_n++]=i-start;
+
+	// check if prepos and get hash
+	ll all=0;
+	s->noprep_n=0;
+	for(int i=0; i<s->words_n ; ++i)
+	{
+		if( !preposCheck(c,s->words_len[i]) )
+		{
+			int sum=0;
+			for(int j=0;j<s->words_len[i];++j)
+				sum = (sum*WordMult+c[j])%WordMod;
+			all = (all*HashMult+sum )%HashMod;
+			s->noprep_num[s->noprep_n++]= i;
+		}
+		c += s->words_len[i] + 1;
+	}
+	
+	//return hash
+	s->noprep_num[s->noprep_n]= s->words_n ; // for code easier
+	return (int)all;
+
 }
 
 struct Link
 {
-	Sentence *str;
+	Sentence *sent;
 	Link *next;
-}*nextp =new Link[DataMax],*hash_table[100000000];
+}*nextlk =new Link[DataMax];
 
-int sentHash(char *&c)// modify origin string
+struct Linkhash
 {
-	int i,sum=0;
-	ll all=0;
+	Link *lk;
+	Linkhash *next;
+}*nextlkh =new Linkhash[DataMax],*hash_table[100000000];
 
-	for(i=0;c[i]!='\t';++i)
+Linkhash* linkFind(int &hash,Sentence *s)
+{
+	Linkhash *lkh = hash_table[hash];
+	while(lkh)
 	{
-		if( c[i] != ' ' )
-			sum = (sum*WordMult+c[i])%WordMod;
-		else 
-		{
-			all = (all*HashMult+sum )%HashMod;
-			sum = 0;
-		}
+		if( sentEq(lkh->lk->sent , s) )
+			return lkh;
+		lkh = lkh->next;
 	}
-	all = (all*HashMult+sum )%HashMod;
-	c = c+i;
-	return (int)all;
+	return NULL;
 }
+
 
 void fileTohashtable()
 {
@@ -61,311 +191,124 @@ void fileTohashtable()
 		while( fgets(ori_data->str,220,f) )
 		{
 			++ori_n;
-			char *c = ori_data->str;
-			int h = sentHash(c);
-
-			// get fre and modify
+			int h = sentDeal(ori_data);
+			
+			// get feq
+			char *c=&ori_data->str[ ori_data->words_pos[ ori_data->words_n-1 ]+
+					               ori_data->words_len[ ori_data->words_n-1 ]];
 			*(c++) = '\0';
 			int dig=0;
 			for(int i=0;c[i]!='\n';++i)
 				dig = dig*10 + c[i]-'0';
 			ori_data->feq = dig;
-
+			
 			// put into hashtable
-			nextp->str  = ori_data++;
-			nextp->next = hash_table[h];
-			hash_table[h]= nextp++ ;
+			Linkhash *lkh = linkFind(h,ori_data);
+			if( !lkh )
+			{
+				lkh = nextlkh;
+				nextlkh->next = hash_table[h] ;
+				hash_table[h] = nextlkh++ ;
+			}
+				
+			nextlk->sent  = ori_data++;
+			nextlk->next = lkh->lk ;
+			lkh->lk = nextlk++ ;
 		}
 		fclose(f);
 	}
 }
 
-const int prepos_n=20;
-std::unordered_set<std::string> prepos_s;
-//Word prepos_v[prepos_n]; i can't put it here QQ
+std::vector< Sentence * > ans;
 
-struct Word // this is use for shallow copy  be aware!!
+bool preposSame(Sentence *a,Sentence *b,int na,int nb)
 {
-	char *str,isprepos;
-	int  hash;
-	int  len ;
-	Word():str(NULL){};
-	~Word()
-	{
-		if(str) 
-			delete[] str;
-	}
+	return a->words_len[na] == b->words_len[nb] &&
+		a->str[ a->words_pos[na]   ] == b->str[ b->words_pos[nb]   ] &&
+		a->str[ a->words_pos[na]+1 ] == b->str[ b->words_pos[nb]+1 ] ;
+}
 
-	void wordNew(char *c,int l)
-	{
-		len = l;
-		hash=0;
-		
-		str = new char[len+1];
-		for(int i=0;i<len;++i)
+bool isEd2(Sentence *a,Sentence *b)// b is input
+{
+	if( a->words_n < b->words_n || a->words_n > b->words_n+2)
+		return 0;
+	int nowa=0,nownum=0;
+	int dis=0;
+	for(int nowb=0;nowb<b->words_n && dis>2;++nowb)
+		if( nowb == b->noprep_num[nownum] )// a is more than b
 		{
-			hash = (hash*WordMult+c[i])%WordMod;
-			str[i] = c[i];
+			dis += a->noprep_num[nownum] - nowa;
+			nowa = a->noprep_num[nownum]+1;
+			++nownum;
 		}
-		str[len]='\0';
-		isprepos = (char)prepos_s.count(str);
-	}
-};
-
-Word prepos_v[prepos_n];
-
-void wordSwap(Word &a,Word &b)
-{
-	Word c;
-	c=a;a=b;b=c;
-	c.str = NULL; //avoid unwanted deletion
-}
-
-struct Sepword
-{
-	Word word[8];
-	int  len,hash;
-	
-	void allPrint()
-	{
-		for(int i=0;i<len;++i)
-			printf("%s:",word[i].str);
-		puts("");
-//		for(int i=0;i<len;++i)
-//			printf("%d:",word[i].len);
-//		puts("");
-		for(int i=0;i<len;++i)
-			printf("%d:",word[i].hash);
-		puts("");
-//		for(int i=0;i<len;++i)
-//			printf("%d:",word[i].isprepos);
-//		puts("");
-		printf("%d\n",hash);
-		printf("%d\n",hashGet());
-	}
-	
-	int hashGet()
-	{
-		ll h=0;
-		for(int i=0;i<len;++i)
-			h = (h*HashMult+word[i].hash)%HashMod;
-		return hash = (int)h;
-	}
-	
-	void inputSep(char *c)
-	{
-		len=0;
-		int start=-1;
-		for(int i=0;c[i];++i)
+		else
 		{
-			if( !isalpha(c[i]) )
-				continue;
-			int start=i;
-			while( isalpha(c[i]) )
-				++i;
-			word[len++].wordNew(c+start,i-start);
-			if(!c[i])break;// be careful for last one
+			// find if b can match to a exclude add one
+			for(nowa ; nowa < a->noprep_num[nownum] ;++nowa)
+				if( preposSame(a,b,nowa,nowb) )
+					break;
+				else 
+					++dis;
+			if( nowa == a->noprep_num[nownum] )
+				return 0; // can't find match one
+			++nowa;
 		}
-		hashGet();
-	}
 
-	bool operator == (const char *c) const
-	{
-		int k=0;
-		for(int i=0;i<len;++i)
-		{
-			for(int j=0;word[i].str[j];++j)
-				if(word[i].str[j]!=c[k++])
-					return false;
-			if(i!=len-1 && c[k++]!=' ')
-				return false;
-		}
-		return c[k]=='\0';
-	}
-
-};
-
-Sentence* findInhashtable(Sepword &s)
-{
-	Link *now = hash_table[s.hash];
-	while( now )
-	{
-		if( s == now->str->str)
-			return now->str;
-		now = now->next;
-	}
-	return NULL;
+	return dis<=2 ;
 }
 
-std::vector<Sentence*> ans;
-void findToans(Sepword &s)
+bool canInsert(Sentence *a,Sentence *b)
 {
-	Sentence* getsent = findInhashtable(s);
-	if( getsent)
-		ans.push_back(getsent);
+	int preva= -1, prevb= -1 ;
+//	a->noprep_num[ a->noprep_n ]= a->words_n;  i add before
+	for(int num=0;num<=b->noprep_n;++num)
+	{
+		if(    prevb +1 == b->noprep_num[num] //no operator between
+			&& preva +1 != a->noprep_num[num] )
+				return 0;
+		preva= a->noprep_num[num];
+		prevb= b->noprep_num[num];
+	}
+	return 1;
 }
 
-void onlyAdd(Sepword &s,int dis=0)
+void sentFilt(Linkhash *lkh,Sentence *s,bool(*check)(Sentence *,Sentence *))
 {
-	findToans(s);
-//	s.allPrint();
-	if(s.len==0 || dis==2) return ;
-	ll hm = 1;
-	ll h= s.hash;
-
-	//add
-	if( s.len>=7)
-		return;
-	++s.len;
-	hm=1;
-	for(int i=s.len-1;i>=0;--i)
+	if(!lkh)return ;
+	Link *lk = lkh->lk ;
+	while( lk )
 	{
-		s.word[i+1] = s.word[i] ;
-		s.word[i].hash=0;
-		h = s.hashGet();
-//		if( (i && s.word[i-1].isprepos) || (i+1<s.len && s.word[i+1].isprepos))
-			for(int j=0;j<prepos_n;++j)
-			{
-				s.word[i] = prepos_v[j];
-				s.hash = (h+hm*s.word[i].hash)%HashMod;
-				onlyAdd(s,dis+1);
-			}
-		hm = hm * HashMult % HashMod;
-	}
-	for(int i=0;i<s.len;++i)
-		s.word[i] = s.word[i+1] ;
-	--s.len;
-}
-
-void edMake(Sepword &s)
-{
-	findToans(s);
-//	s.allPrint();
-	if(s.len==0 ) return ;
-	Word tmp;
-	ll hm = 1;
-	ll h= s.hash;
-	
-	//delete
-	--s.len;
-	for(int i=s.len;i>=0;--i)
-	{
-		wordSwap( tmp , s.word[i] );
-		s.hashGet();
-		if(tmp.isprepos)
-			findToans(s);
-//			edMake(s,dis+1);
-	}
-	for(int i=0;i<=s.len;++i)
-		wordSwap( tmp , s.word[i] );
-	++s.len;
-
-	//substitude
-	for(int i=s.len-1;i>=0;--i)
-	{
-		h = ((h-hm*s.word[i].hash)%HashMod+HashMod)%HashMod;
-		tmp = s.word[i] ;
-		if(tmp.isprepos)
-			for(int j=0;j<prepos_n;++j)
-			{
-				s.word[i] = prepos_v[j];
-				s.hash = (h+(ll)s.word[i].hash*hm)%HashMod;
-				findToans(s);
-//				edMake(s,dis+1);
-			}
-		s.word[i] = tmp ;
-		h = (h+hm*s.word[i].hash)%HashMod;
-		hm = hm * HashMult % HashMod;
-	}
-	tmp.str = NULL; //avoid unwanted deletion
-
-	//add
-	if( s.len>=7)
-		return;
-	++s.len;
-	hm=1;
-	for(int i=s.len-1;i>=0;--i)
-	{
-		s.word[i+1] = s.word[i] ;
-		s.word[i].hash=0;
-		h = s.hashGet();
-		if( (i && s.word[i-1].isprepos) || (i+1<s.len && s.word[i+1].isprepos))
-			for(int j=0;j<prepos_n;++j)
-			{
-				s.word[i] = prepos_v[j];
-				s.hash = (h+hm*s.word[i].hash)%HashMod;
-				findToans(s);
-//				edMake(s,dis+1);
-			}
-		hm = hm * HashMult % HashMod;
-	}
-	for(int i=0;i<s.len;++i)
-		s.word[i] = s.word[i+1] ;
-	--s.len;
-}
-
-void preposInit()
-{
-	char prepos_c[prepos_n][10]= {
-		"of", "to", "in", "for", "with", 
-		"on", "at","by", "from", "up", 
-		"about", "than", "after", "before", "down", 
-		"between", "under", "since", "without", "near"};
-
-	for(int i=0;i<prepos_n;++i)
-	{
-		prepos_v[i].wordNew(prepos_c[i],strlen(prepos_c[i]));
-		prepos_s.insert(prepos_c[i]);
+		if( check( lk->sent, s) )
+			ans.push_back(lk->sent);
+		lk = lk->next;
 	}
 }
 
-void ansPrint()
-{
-	std::sort(ans.begin(),ans.end(),sentComp);
-	if(ans.size()==0)
-	{
-		puts("output: 0");
-		return ;
-	}
-	int n=1;
-	for(int i=1;i<ans.size() && n<10;++i)// clear the same item
-		if( ans[i-1] != ans[i])
-			ans[n++] = ans[i];
-	if(n>10)
-		n=10;
-	printf("output: %d\n",n);
-	for(int i=0;i<n;++i)
-		printf("%s\t%d\n",ans[i]->str,ans[i]->feq);
-}
-
-bool hasprepos(Sepword &s)
-{
-	for(int i=0;i<s.len;++i)
-		if(s.word[i].isprepos)
-			return 1;
-	return 0;
-}
-	
 int main()
 {
 	preposInit();
 	fileTohashtable();
-	char c[1000];
-	while( fgets(c,100,stdin) )
+	stop = '\n';
+	Sentence *s = new Sentence;
+	while( fgets(s->str,220,stdin) )
 	{
-		printf("query: %s",c);
-		Sepword s;
-		s.inputSep(c);
+		int h = sentDeal(s);
+		Linkhash *lkh = linkFind(h,s);
 		ans.clear();
 
-		// check if it has prepos
-		if(hasprepos(s))
-			edMake(s);
+		if( s->words_n == s->noprep_n )//no prep  
+			sentFilt( lkh ,s ,isEd2	   );
 		else
-			onlyAdd(s,0);
-
-		ansPrint();
+			sentFilt( lkh ,s ,canInsert);
 		
+		std::sort(ans.begin(),ans.end(),sentComp);
+		if(ans.size()>10)
+			ans.resize(10);
+		// query
+		printf("query: %s",s->str);
+		printf("output: %d\n",ans.size());
+		for(int i=0;i<ans.size();++i)
+			printf("%s\t%d\n",ans[i]->str,ans[i]->feq);
 	}
 	return 0;
 }
--- project/read.cpp
+++ project/read.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 
-#define WordLen 40
-#define DataMax 50000000
+//#define DataMax 50000000
+#define DataMax 500
 #define HashMult 2350001
 #define HashMod 99999989
 #define WordMult 11261
@@ -10,38 +10,175 @@
 #define ll long long 
 using std::string;
 
+struct Trie
+{
+	bool exist;
+	Trie* ch[26];
+	Trie():exist(0){
+		memset(ch,0,sizeof(ch));
+	}
+
+
+}*root;
+
+void preposInsert(char *str)
+{
+	Trie *now = root;
+	for(int i=0;str[i];++i)
+	{
+		char c = str[i]-'a';
+		if( now->ch[c] )
+			now = now->ch[c];
+		else
+			now = now->ch[c] = new Trie;
+	}
+	now->exist=1;
+}
+
+bool preposCheck(char *str,int& len)
+{
+	Trie *now = root;
+	for(int i=0;i<len;++i)
+	{
+		now = now->ch[str[i]-'a'];
+		if(!now) return 0;
+	}
+	return now->exist;
+}
+
+void preposInit()
+{
+	const int prepos_n = 20;
+	char prepos_c[prepos_n][10]= {
+		"of", "to", "in", "for", "with", 
+		"on", "at","by", "from", "up", 
+		"about", "than", "after", "before", "down", 
+		"between", "under", "since", "without", "near"};
+
+	root = new Trie;
+	for(int i=0;i<prepos_n;++i)
+		preposInsert(prepos_c[i]);
+}
+
 struct Sentence
 {
 	char str[220];
 	int feq;
+
+	int noprep_n,
+		noprep_num[6];
+	
+	int words_n,
+		words_pos[10],
+		words_len[10];
+
+	void sentPrint()
+	{
+		puts("----------------------");
+		printf("n%d\n",noprep_n);
+		for(int i=0;i<words_n;++i)
+			printf("%d %d\n",words_pos[i],words_len[i]);
+		for(int i=0;i<noprep_n;++i)
+			printf("%d\n",noprep_num[i]);
+		printf("%s\t%d\n",str,feq);
+	}
+
+	friend bool sentEq(const Sentence *a,const Sentence *b)
+	{
+		if(a->noprep_n != b->noprep_n)
+			return 0;
+		for(int i=0;i<a->noprep_n;++i)
+		{
+			if(a->words_len[a->noprep_num[i]]!=b->words_len[b->noprep_num[i]])
+				return 0;
+			int len = a->words_len[a->noprep_num[i]];
+			int pos = a->words_pos[a->noprep_num[i]],
+				posb= b->words_pos[b->noprep_num[i]];
+			for(int j=0;j<len;++j)
+				if(a->str[pos+j] != b->str[posb+j])
+					return 0;
+		}
+		return 1;
+	}
+
+	bool operator < (const Sentence *&b)const
+	{
+		return feq > b->feq;
+	}
+
 }*ori_data = new Sentence [DataMax];
 int ori_n;
 
-struct Link
-{
-	Sentence *str;
-	Link *next;
-}*nextp =new Link[DataMax],*hash_table[100000000];
 
-int sentHash(char *&c)// modify origin string
+char stop='\t';
+int sentDeal(Sentence *s)// modify origin string
 {
-	int i,sum=0;
-	ll all=0;
-
-	for(i=0;c[i]!='\t';++i)
-	{
-		if( c[i] != ' ' )
-			sum = (sum*WordMult+c[i])%WordMod;
+	char *c = s->str;
+	
+	// get the size of each word
+	s->words_n = 0;
+	int start=-1,i;
+	for(i=0;c[i]!=stop;++i)
+		if( c[i] != ' ')
+		{
+			if(start==-1)
+				start=i;
+		}
 		else 
 		{
-			all = (all*HashMult+sum )%HashMod;
-			sum = 0;
+			s->words_pos[s->words_n  ]=  start;
+			s->words_len[s->words_n++]=i-start;
+			start=-1;
 		}
+	s->words_pos[s->words_n  ]=  start;
+	s->words_len[s->words_n++]=i-start;
+
+	// check if prepos and get hash
+	ll all=0;
+	s->noprep_n=0;
+	for(int i=0; i<s->words_n ; ++i)
+	{
+		if( !preposCheck(c,s->words_len[i]) )
+		{
+			int sum=0;
+			for(int j=0;j<s->words_len[i];++j)
+				sum = (sum*WordMult+c[j])%WordMod;
+			all = (all*HashMult+sum )%HashMod;
+			s->noprep_num[s->noprep_n++]= i;
+		}
+		c += s->words_len[i] + 1;
 	}
-	all = (all*HashMult+sum )%HashMod;
-	c = c+i;
+	
+	//return hash
+	s->noprep_num[s->noprep_n]= s->words_n ; // for code easier
 	return (int)all;
+
 }
+
+struct Link
+{
+	Sentence *sent;
+	Link *next;
+}*nextlk =new Link[DataMax];
+
+struct Linkhash
+{
+	Link *lk;
+	Linkhash *next;
+}*nextlkh =new Linkhash[DataMax],*hash_table[100000000];
+
+Linkhash* linkFind(int &hash,Sentence *s)
+{
+	Linkhash *lkh = hash_table[hash];
+	while(lkh)
+	{
+		if( sentEq(lkh->lk->sent , s) )
+			return lkh;
+		lkh = lkh->next;
+	}
+	return NULL;
+}
+
 
 void fileTohashtable()
 {
@@ -54,20 +191,29 @@ void fileTohashtable()
 		while( fgets(ori_data->str,220,f) )
 		{
 			++ori_n;
-			char *c = ori_data->str;
-			int h = sentHash(c);
-
-			// get fre and modify
+			int h = sentDeal(ori_data);
+			
+			// get feq
+			char *c=&ori_data->str[ ori_data->words_pos[ ori_data->words_n-1 ]+
+					               ori_data->words_len[ ori_data->words_n-1 ]];
 			*(c++) = '\0';
 			int dig=0;
 			for(int i=0;c[i]!='\n';++i)
 				dig = dig*10 + c[i]-'0';
 			ori_data->feq = dig;
-
+			
 			// put into hashtable
-			nextp->str  = ori_data++;
-			nextp->next = hash_table[h];
-			hash_table[h]= nextp++ ;
+			Linkhash *lkh = linkFind(h,ori_data);
+			if( !lkh )
+			{
+				lkh = nextlkh;
+				nextlkh->next = hash_table[h] ;
+				hash_table[h] = nextlkh++ ;
+			}
+				
+			nextlk->sent  = ori_data++;
+			nextlk->next = lkh->lk ;
+			lkh->lk = nextlk++ ;
 		}
 		fclose(f);
 	}
@@ -75,7 +221,17 @@ void fileTohashtable()
 
 int main()
 {
-	fileTohashtable();
+	preposInit();
+//	fileTohashtable();
+//	return 0;
+	Sentence s;
+	stop = '\n';
+	while( fgets(s.str,100,stdin) )
+	{
+		printf("%d\n", sentDeal(&s));
+		s.str[ s.words_pos[ s.words_n-1 ] + s.words_len[ s.words_n-1 ] ] ='\0';
+		s.sentPrint();
+	}
 	return 0;
 }
 
